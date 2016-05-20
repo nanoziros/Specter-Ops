d3798b910db10244ad34f9f89b7c9bf343dbb7ac
nanoziros <nero.lucho92@gmail.com>
2016-05-20 18:51:52 -0400

    Animate collectables text

--- Assets/Scripts/Gameplay Props/Collectables/CollectableController.cs
+++ Assets/Scripts/Gameplay Props/Collectables/CollectableController.cs
@@ -4,18 +4,62 @@ namespace SpecterOps
     /// <summary>
     /// Manages all collectable elements in the game
     /// </summary>
+    [RequireComponent(typeof(Animator))]
     public class CollectableController : GameplayElementController
     {
         // Score awarded to the player on collection
         public int CollectableValue { get; private set; }
+
+        // Animation controllers
+        private Animator collectableAnimator;
+
+        // Control parameters
+        private bool isInitialized = false;
 
         /// <summary>
         /// Use this for initialization
         /// </summary>
         public void Initialize(int collectableValue)
         {
+            // Get component references
+            this.collectableAnimator = this.GetComponent<Animator>();
+
+            // Subscribe to proper game events
+            GamePresenter.Instance.GamePaused += this.PauseAnimation;
+            GamePresenter.Instance.GameResumed += this.ResumeAnimation;
+
             // Set initial parameters
             this.CollectableValue = collectableValue;
+
+            // Set initialized flag
+            this.isInitialized = true;
+        }
+
+        /// <summary>
+        /// Since we did some event subscribing, we need to safely unsubscribe on disable and on destroy
+        /// </summary>
+        private void OnDisable()
+        {
+            // Check if we initialized this class
+            if (!this.isInitialized)
+                return;
+
+            // Remove initialization flag
+            this.isInitialized = false;
+
+            // Unsubscribe gameflow events
+            GamePresenter.Instance.GamePaused -= this.PauseAnimation;
+            GamePresenter.Instance.GameResumed -= this.ResumeAnimation;
+        }
+        private void OnDestroy()
+        {
+            // Check if we initialized this class
+            if (!this.isInitialized)
+                return;
+
+            // Unsubscribe gameflow events
+            GamePresenter.Instance.GamePaused -= this.PauseAnimation;
+            GamePresenter.Instance.GameResumed -= this.ResumeAnimation;
         }
 
         /// <summary>
@@ -34,6 +78,21 @@ namespace SpecterOps
 
             // Deactivate gameobject
             this.gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// Pause animator
+        /// </summary>
+        public void PauseAnimation()
+        {
+            this.collectableAnimator.enabled = false;
+        }
+        /// <summary>
+        /// Resume animator
+        /// </summary>
+        public void ResumeAnimation()
+        {
+            this.collectableAnimator.enabled = true;
         }
     }
 }
---
+++ Assets/Scripts/UI/Animation/AutoScalerOnValueChange.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+
+namespace SpecterOps.Utilities
+{
+    using UnityEngine;
+    using System.Collections;
+    using UnityEngine.UI;
+
+    using  DG;
+
+    /// <summary>
+    /// This class executes a pop up animation on a text element when its value changes (thanks to UGUI's input field component, its OnValueChange can
+    /// call the method on this class)
+    /// </summary>
+    public class AutoScalerOnValueChange : MonoBehaviour
+    {
+        // Animation parameters
+        public float InitialScale = 1.5f;
+        public float FinalScale = 1.0f;
+        public float AnimationDuration = 0.5f;
+        public Ease AnimationEase = Ease.InBounce;
+        private Tween animationTween;
+
+        /// <summary>
+        /// Execute de pop up animation using dotween
+        /// </summary>
+        public void ExecutePopUpAnimation()
+        {
+            // Kill tween if it was executing before
+            if (this.animationTween != null && this.animationTween.IsPlaying())
+                this.animationTween.Kill();
+
+            // Play tween
+            this.transform.localScale = Vector3.one*this.InitialScale;
+            this.animationTween =
+                this.transform.DOScale(this.FinalScale, this.AnimationDuration).SetEase(this.AnimationEase);
+        }
+    }
+}
