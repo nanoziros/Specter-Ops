3adab5ac6c1e49fee1cb58fb0811f94083f2a7b2
nanoziros <nero.lucho92@gmail.com>
2016-05-20 20:00:20 -0400

    Invulnerability flickering system

--- Assets/Scripts/Player/PlayerVFXController.cs
+++ Assets/Scripts/Player/PlayerVFXController.cs
@@ -1,4 +1,6 @@
-﻿namespace SpecterOps.Player
+﻿using DG.Tweening;
+
+namespace SpecterOps.Player
 {
     using UnityEngine;
 
@@ -9,6 +11,8 @@
 
         // Invulnerability vfx
         public float InvulnerabilitySpriteAlpha = 0.5f;
+        public float InvulnrabilityVfxInterval = 0.2f;
+        private Sequence invulnerabilitySequence;
 
         /// <summary>
         /// Use this to initialize this instance
@@ -24,10 +28,25 @@
         /// </summary>
         public void ManageInvulnerabilityVFX(bool state)
         {
-            // Set alpha based on invulnerability state
+            // Kill tween if it was executing before
+            if (this.invulnerabilitySequence != null && this.invulnerabilitySequence.IsPlaying())
+                this.invulnerabilitySequence.Kill();
+
+            // Restore alpha
             this.playerSpriteRenderer.color = new Color(this.playerSpriteRenderer.color.r,
                 this.playerSpriteRenderer.color.g, this.playerSpriteRenderer.color.b,
-                state ? this.InvulnerabilitySpriteAlpha : 1);
+                1);
+
+            // Execute animation
+            if (state)
+            {
+                // Generate flickering sequence
+                this.invulnerabilitySequence = DOTween.Sequence().SetLoops(-1);
+                this.invulnerabilitySequence.Append(this.playerSpriteRenderer.DOFade(this.InvulnerabilitySpriteAlpha,
+                    this.InvulnrabilityVfxInterval/2.0f));
+                this.invulnerabilitySequence.Append(this.playerSpriteRenderer.DOFade(1,
+                    this.InvulnrabilityVfxInterval / 2.0f));
+            }
         }
     }
 }
