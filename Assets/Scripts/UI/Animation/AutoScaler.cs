eda90866f7e48796c11c43917df37e839f9e8f39
nanoziros <nero.lucho92@gmail.com>
2016-05-22 13:34:34 -0400

    Auto gameobject scaler integration

---
+++ Assets/Scripts/UI/Animation/AutoScaler.cs
@@ -0,0 +1,36 @@
+﻿namespace SpecterOps.Utilities
+{
+    using UnityEngine;
+    using DG.Tweening;
+    using UnityEngine.UI;
+
+    public class AutoScaler : MonoBehaviour
+    {
+
+        // AnimationParameters
+        public float OriginScale = 1.0f;
+        public float TargetScale = 0.75f;
+        public float AnimationInterval = 0.2f;
+        public float DelayBetweenScaling= 0.0f;
+        private Sequence scalingSequence;
+
+        // Start scaling on enable
+        private void OnEnable()
+        {
+            // Kill tween if it was executing before
+            if (this.scalingSequence != null && this.scalingSequence.IsPlaying())
+                this.scalingSequence.Kill();
+
+            // Restore scale
+            this.transform.localScale = Vector3.one*this.OriginScale;
+
+            // Generate flickering sequence
+            this.scalingSequence = DOTween.Sequence().SetLoops(-1);
+            this.scalingSequence.Append(this.transform.DOScale(this.TargetScale,
+                this.AnimationInterval / 2.0f));
+            this.scalingSequence.Append(this.transform.DOScale(1,
+                this.AnimationInterval / 2.0f));
+            this.scalingSequence.AppendInterval(this.DelayBetweenScaling);
+        }
+    }
+}
